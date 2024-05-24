dc21803dc1c60e95b9bea8e4e9560a35705da245
henriqueAlgauer <algauer.henrique@gmail.com>
2024-05-24 16:09:59 -0300

    estrutura mvc finalizada

---
+++ testMVC/app/core/config.php
@@ -0,0 +1,26 @@
+<?php 
+
+if($_SERVER['SERVER_NAME'] == 'localhost'){
+    // databse config
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+    define('DBPORT', '3307');
+
+    define('ROOT', 'http://localhost/testphp/mvc/public');
+}else{
+    define('ROOT', 'https://info.ghdigitalservices.com.br');
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '1234');
+    define('DBDRIVER', '');
+}
+
+define('APP_NAME', 'My Website');
+define('APP_DESC', 'The desc');
+
+/* true means show errors */
+define('DEBUG', true);
