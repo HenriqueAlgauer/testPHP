d6f445656d836ae16989025fd922df9581c2c1b0
Pia <algauer.henrique@gmail.com>
2024-06-02 21:32:15 -0300

    produtos_editar pronto

--- testMVC-Login/app/views/produto_adicionar.view.php
+++ testMVC-Login/app/views/produto_adicionar.view.php
@@ -15,26 +15,25 @@
     <?= menu() ?>
     <main class="mx-auto">
         <div class="text-center p-5">
-            <h1>Adicionar Produtos</h1>
+            <h1>Adicionar Produto</h1>
         </div>
         <div class="p-5 d-flex justify-content-center">
             <form class=" w-50 d-flex flex-column" method="post">
 
                 <div class="input-group mb-3">
                     <span class="input-group-text" for="nomeProduto" id="basic-addon1">Nome</span>
-                    <input class="form-control" type="text" name="nome" aria-label="Username"
-                        aria-describedby="basic-addon1" id="nome">
+                    <input type="text" name="nome" class="form-control" aria-describedby="basic-addon1" id="nome">
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" for="nomeProduto" id="basic-addon1">Preço</span>
                     <input class="form-control" type="number" step="0.01" min="0" name="preco" id="preco"
-                        aria-label="Username" aria-describedby="basic-addon1">
+                        aria-describedby="basic-addon1">
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" for="nomeProduto" id="basic-addon1">Estoque</span>
                     <input class="form-control" type="number" name="estoque" id="estoque">
                 </div>
-                <button class="btn btn-success" type="submit">Adicionar produto</button>
+                <button class="mx-auto btn btn-success w-50" type="submit">Adicionar produto</button>
             </form>
         </div>
     </main>
--- testMVC-Login/app/views/produto_editar.view.php
+++ testMVC-Login/app/views/produto_editar.view.php
@@ -6,41 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="<?= ROOT ?>/assets/img/php3d.png" type="image/x-icon" />
     <title>Dashboard</title>
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/reset.css" />
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
 </head>
 
-<body>
-    <?=menu()?>
-    <main>
-
-        <div class="blur">
+<body class="container">
+    <?= menu() ?>
+    <main class="mx-auto">
+        <div class="text-center p-5">
             <h1>Produtos Editar</h1>
         </div>
-        <br><br>
-        <div class="internalNav">
-            <form method="post" action="<?= ROOT ?>/produto">
-                <input type="search" placeholder="digite produto" id="buscarProduto" name="buscarProduto">
-                <button type="submit">Buscar</button>
+        <div class="p-5 d-flex justify-content-center">
+            <form class=" w-50 d-flex flex-column" method="post">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" for="nomeProduto" id="basic-addon1">Nome</span>
+                    <input class="form-control" aria-describedby="basic-addon1" type="text" name="nome" id="nome"
+                        value="<?= $produto->nome ?>">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" for="nomeProduto" id="basic-addon1">Preço</span>
+                    <input class="form-control" aria-describedby="basic-addon1" type="number" step="0.01" min="0"
+                        name="preco" id="preco" value="<?= $produto->preco ?>">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" for="nomeProduto" id="basic-addon1">Estoque</span>
+                    <input class="form-control" aria-describedby="basic-addon1" type="number" name="estoque"
+                        id="estoque" value="<?= $produto->estoque ?>">
+                </div>
+
+                <button class="mx-auto btn btn-success" type="submit">Aplicar Alterações</button>
             </form>
-            <button type="submit">Adicionar Produto</button>
-            <a href="<?=ROOT?>/produto">Voltar pagina</a>
         </div>
-
-        <form method="post">
-            <label for="nomeProduto">Nome</label><br>
-            <input type="text" name="nome" id="nome" value="<?= $produto->nome ?>">
-            <br><br>
-            <label for="nomeProduto">Preco</label> <br>
-            <input type="number" step="0.01" min="0" name="preco" id="preco" value="<?= $produto->preco ?>">
-            <br><br>
-            <label for="nomeProduto">Estoque</label><br>
-            <input type="number" name="estoque" id="estoque" value="<?= $produto->estoque ?>">
-
-            <button type="submit">Enviar Alterações</button>
-        </form>
+        <a href="<?=ROOT?>/produto">Voltar pagina</a>
     </main>
-
     <?=footer()?>
 
 </body>
