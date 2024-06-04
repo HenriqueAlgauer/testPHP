edbb665d7f3ed92ffd668523f60c19f7e5705135
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-06-04 19:25:16 -0300

    ajuste de formatação view venda_adicionar

--- testMVC-Login/app/views/venda_adicionar.view.php
+++ testMVC-Login/app/views/venda_adicionar.view.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
@@ -14,23 +14,22 @@ if (empty($_SESSION['csrf_token'])) {
     <title>Adicionar Venda</title>
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/style.css" />
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/financeiro.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
 </head>
 
 <body class="container">
     <?= menu() ?>
     <main class="mx-auto ">
-        <?php if (isset($error) && !empty($error)): ?>
-        <div class="w-50 mb-4 mx-auto alert alert-danger">
-            <?= $error ?>
-        </div>
+        <?php if (isset($error) && !empty($error)) : ?>
+            <div class="w-50 mb-4 mx-auto alert alert-danger">
+                <?= $error ?>
+            </div>
         <?php endif; ?>
 
-        <?php if (isset($success) && !empty($success)): ?>
-        <div class="w-50 mb-4 mx-auto alert alert-success">
-            <?= $success ?>
-        </div>
+        <?php if (isset($success) && !empty($success)) : ?>
+            <div class="w-50 mb-4 mx-auto alert alert-success">
+                <?= $success ?>
+            </div>
         <?php endif; ?>
         <div class="shadow rounded py-5">
             <form id="vendaForm" class="px-5 d-flex justify-content-between" method="post">
@@ -44,16 +43,14 @@ if (empty($_SESSION['csrf_token'])) {
                     <div>
                         <h2>Produtos Selecionados</h2>
                     </div>
-                    <div class="d-flex flex-column justify-content-between align-items-center"
-                        id="produtos-selecionados">
+                    <div class="d-flex flex-column justify-content-between align-items-center" id="produtos-selecionados">
                         <ul id="selectedProductsList" class="list-group w-100"></ul>
                     </div>
                 </div>
                 <div class="w-25 d-flex flex-column justify-content-between">
                     <div class="d-flex align-items-center gap-2 mb-5">
                         <label for="formaPagamento">Forma De Pagamento</label>
-                        <select style='max-width:120px' class="form-select" name="formaPagamento" id="formaPagamento"
-                            aria-label="Default select example">
+                        <select style='max-width:120px' class="form-select" name="formaPagamento" id="formaPagamento" aria-label="Default select example">
                             <option value="credito">Credito</option>
                             <option value="debito">Debito</option>
                             <option value="dinheiro">Dinheiro</option>
@@ -70,13 +67,6 @@ if (empty($_SESSION['csrf_token'])) {
             </form>
 
             <div class="px-5 d-flex flex-column justify-content-center align-items-start">
-                <!-- <form class="mt-5" method="post">
-                    <div class="input-group mb-3">
-                        <input class="form-control" type="search" placeholder="nome do produto" id="buscarProduto"
-                            name="buscarProduto">
-                        <button class="btn btn-outline-secondary" type="submit">Buscar</button>
-                    </div>
-                </form> -->
 
                 <table class="border border-1 shadow-sm table table-hover mt-5 mx-auto">
                     <thead>
@@ -89,27 +79,25 @@ if (empty($_SESSION['csrf_token'])) {
                         </tr>
                     </thead>
                     <tbody id="productTableBody">
-                        <?php if (isset($produtos) && is_array($produtos) && count($produtos) > 0): ?>
-                        <?php foreach ($produtos as $produto): ?>
-                        <tr>
-                            <td><strong><?php echo $produto->id; ?></strong></td>
-                            <td><?php echo $produto->nome; ?></td>
-                            <td>R$ <?php echo $produto->preco; ?></td>
-                            <td><?php echo $produto->estoque; ?></td>
-                            <td>
-                                <div class="d-flex pe-5 justify-content-between">
-                                    <input style="max-width: 120px;" class="form-control" type="number"
-                                        name="quantidade" placeholder="Qnt." min="1">
-                                    <button type="button" class="btn btn-primary"
-                                        onclick="addProduct(<?php echo $produto->id; ?>, '<?php echo $produto->nome; ?>', <?php echo $produto->preco; ?>, this.previousElementSibling.value)">Adicionar</button>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                        <?php else: ?>
-                        <tr>
-                            <td colspan="5">Nenhum produto encontrado.</td>
-                        </tr>
+                        <?php if (isset($produtos) && is_array($produtos) && count($produtos) > 0) : ?>
+                            <?php foreach ($produtos as $produto) : ?>
+                                <tr>
+                                    <td><strong><?php echo $produto->id; ?></strong></td>
+                                    <td><?php echo $produto->nome; ?></td>
+                                    <td>R$ <?php echo $produto->preco; ?></td>
+                                    <td><?php echo $produto->estoque; ?></td>
+                                    <td>
+                                        <div class="d-flex pe-5 justify-content-between">
+                                            <input style="max-width: 120px;" class="form-control" type="number" name="quantidade" placeholder="Qnt." min="1">
+                                            <button type="button" class="btn btn-primary" onclick="addProduct(<?php echo $produto->id; ?>, '<?php echo $produto->nome; ?>', <?php echo $produto->preco; ?>, this.previousElementSibling.value)">Adicionar</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td colspan="5">Nenhum produto encontrado.</td>
+                            </tr>
                         <?php endif; ?>
                     </tbody>
                 </table>
@@ -122,22 +110,22 @@ if (empty($_SESSION['csrf_token'])) {
     <?= footer() ?>
 
     <script>
-    function addProduct(id, nome, preco, quantidade) {
-        if (quantidade <= 0) {
-            alert('Por favor, insira uma quantidade válida.');
-            return;
-        }
+        function addProduct(id, nome, preco, quantidade) {
+            if (quantidade <= 0) {
+                alert('Por favor, insira uma quantidade válida.');
+                return;
+            }
 
-        const product = {
-            id,
-            nome,
-            preco,
-            quantidade
-        };
-        const productItem = document.createElement('li');
-        productItem.className =
-            'list-group-item w-100 my-1 d-flex justify-content-between align-items-center item';
-        productItem.innerHTML = `
+            const product = {
+                id,
+                nome,
+                preco,
+                quantidade
+            };
+            const productItem = document.createElement('li');
+            productItem.className =
+                'list-group-item w-100 my-1 d-flex justify-content-between align-items-center item';
+            productItem.innerHTML = `
             <div>
                 <strong>ID:</strong> ${product.id} |
                 <strong>Nome:</strong> ${product.nome} |
@@ -147,65 +135,65 @@ if (empty($_SESSION['csrf_token'])) {
             <input type="hidden" name="valor${id}" value="${preco}">
             <input type="hidden" name="quantidade${id}" value="${quantidade}">
         `;
-        document.getElementById('selectedProductsList').appendChild(productItem);
-        calculateTotal();
-    }
-
-    function calculateTotal() {
-        const items = document.querySelectorAll('.item');
-        let total = 0;
-
-        items.forEach(item => {
-            const valor = item.querySelector('[name^="valor"]').value;
-            const quantidade = item.querySelector('[name^="quantidade"]').value;
-
-            if (valor && quantidade) {
-                total += parseFloat(valor) * parseInt(quantidade);
-            }
-        });
-        document.getElementById('totalPrice').textContent = total.toFixed(2);
-    }
-
-    function removeLastItem() {
-        const list = document.getElementById('selectedProductsList');
-        if (list.lastElementChild) {
-            list.removeChild(list.lastElementChild);
+            document.getElementById('selectedProductsList').appendChild(productItem);
             calculateTotal();
         }
-    }
 
-    function submitVendaForm() {
-        const form = document.getElementById('vendaForm');
-        const selectedProducts = document.querySelectorAll('.item');
-        const formaPagamento = document.getElementById('formaPagamento').value;
-        const totalPrice = document.getElementById('totalPrice').textContent;
+        function calculateTotal() {
+            const items = document.querySelectorAll('.item');
+            let total = 0;
 
-        const vendaData = {
-            formaPagamento,
-            totalPrice,
-            items: []
-        };
+            items.forEach(item => {
+                const valor = item.querySelector('[name^="valor"]').value;
+                const quantidade = item.querySelector('[name^="quantidade"]').value;
 
-        selectedProducts.forEach(item => {
-            const id = item.querySelector('input[name^="valor"]').name.replace('valor', '');
-            const valor = item.querySelector('input[name^="valor"]').value;
-            const quantidade = item.querySelector('input[name^="quantidade"]').value;
-
-            vendaData.items.push({
-                id,
-                valor,
-                quantidade
+                if (valor && quantidade) {
+                    total += parseFloat(valor) * parseInt(quantidade);
+                }
             });
-        });
+            document.getElementById('totalPrice').textContent = total.toFixed(2);
+        }
 
-        const input = document.createElement('input');
-        input.type = 'hidden';
-        input.name = 'vendaData';
-        input.value = JSON.stringify(vendaData);
-        form.appendChild(input);
+        function removeLastItem() {
+            const list = document.getElementById('selectedProductsList');
+            if (list.lastElementChild) {
+                list.removeChild(list.lastElementChild);
+                calculateTotal();
+            }
+        }
 
-        form.submit();
-    }
+        function submitVendaForm() {
+            const form = document.getElementById('vendaForm');
+            const selectedProducts = document.querySelectorAll('.item');
+            const formaPagamento = document.getElementById('formaPagamento').value;
+            const totalPrice = document.getElementById('totalPrice').textContent;
+
+            const vendaData = {
+                formaPagamento,
+                totalPrice,
+                items: []
+            };
+
+            selectedProducts.forEach(item => {
+                const id = item.querySelector('input[name^="valor"]').name.replace('valor', '');
+                const valor = item.querySelector('input[name^="valor"]').value;
+                const quantidade = item.querySelector('input[name^="quantidade"]').value;
+
+                vendaData.items.push({
+                    id,
+                    valor,
+                    quantidade
+                });
+            });
+
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'vendaData';
+            input.value = JSON.stringify(vendaData);
+            form.appendChild(input);
+
+            form.submit();
+        }
     </script>
 </body>
 
